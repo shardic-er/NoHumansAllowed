beacf8b1d9d65de8c81220910fe79f8af3dcaf47
Shardic <eli.ronai@gmail.com>
2023-05-18 23:12:03 -0600

    fixed deprication warning and type guard in music.tsx

--- chat-room-frontend/src/components/Music/Music.tsx
+++ chat-room-frontend/src/components/Music/Music.tsx
@@ -1,26 +1,37 @@
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
-//components/source/chatroomfrontend/nohumansallowed
 interface MusicProps {
     musicSource: string;
 }
 
-function Music({musicSource}: MusicProps): JSX.Element {
-
+function Music({ musicSource }: MusicProps): React.ReactElement {
     const audioRef = useRef<HTMLAudioElement | null>(null);
 
     useEffect(() => {
-        if (audioRef.current) {
-            audioRef.current.src = musicSource;
-            audioRef.current.volume = 0.1;
-            audioRef.current.play();
+        const audio = audioRef.current;
+        if (audio instanceof HTMLAudioElement) {
+            audio.src = musicSource;
+            audio.volume = 0.1;
+            const playPromise = audio.play();
+
+            if (playPromise !== undefined) {
+                playPromise
+                    .then(_ => {
+                        // Automatic playback started!
+                    })
+                    .catch(error => {
+                        // Auto-play was prevented
+                        console.log(error);
+                    });
+            }
         }
     }, [musicSource]);
 
+
+
     return (
-        // remove controls later.
         <audio ref={audioRef} src={musicSource} autoPlay controls loop />
-    )
+    );
 }
 
 export default Music;
