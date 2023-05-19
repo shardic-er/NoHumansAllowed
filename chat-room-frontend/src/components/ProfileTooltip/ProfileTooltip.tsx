85071ec7d4cf84007bd416fa8da58839badb0781
aeharriman <aeharrima@gmail.com>
2023-05-19 16:32:43 -0500

    profiletooltip edits

--- chat-room-frontend/src/components/ProfileTooltip/ProfileTooltip.tsx
+++ chat-room-frontend/src/components/ProfileTooltip/ProfileTooltip.tsx
@@ -3,8 +3,6 @@ import Tooltip from 'react-bootstrap/Tooltip';
 // Add props and types, interface if necessary
 function ProfileTooltip() {
     
-
-    // Make a good looking tooltip here
     return <>
             <Tooltip id="button-tooltip-2">This message will depend on props passed down from the profile{}</Tooltip>
         </>
