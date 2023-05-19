c48b64e78d076b1ae766e96bdc097470785548f4
Shardic <eli.ronai@gmail.com>
2023-05-18 22:41:34 -0600

    added nice looking player portrates and scrollable frame

--- chat-room-frontend/src/components/ActivePlayers/ActivePlayers.tsx
+++ chat-room-frontend/src/components/ActivePlayers/ActivePlayers.tsx
@@ -1,17 +1,55 @@
 import {ListGroup} from "react-bootstrap";
+import {AppUser} from "../../Utils/Interfaces";
+import Profile from "../Profile/Profile";
 
-function ActivePlayers(props:{playerList}){
+function ActivePlayers(props:{playerList:AppUser[]}){
+
+    const playerList = props.playerList
+
+    const isPlayerSpectator = () => {
+        return true
+    }
+
+    const getPlayerCards = () => {
+        return playerList.map(player => {
+            return <Profile
+                imgURL={'src/assets/accuser.png'}
+                isSpectator={isPlayerSpectator()}>
+            </Profile>
+        })
+
+    }
+
+    const containerStyle = {
+        position: 'absolute',
+        top:'14vh',
+        right:'15vw',
+        bottom:'20vh',
+        backgroundColor: 'rgba(50, 50, 50, 1)',
+        border: 'solid',
+        borderColor: 'rgba(20,20,20,1)',
+        borderRadius: '10px',
+        padding: '5px',
+        // maxHeight: `calc(100vh - 30vh)`,
+        overflow: 'hidden',
+        maxWidth:'128px'
+    };
+
+    const scrollableWithoutBar = {
+        maxHeight: '100%',  // Adjust this value based on your needs
+        overflowY: 'auto',
+        scrollbarWidth: 'none',  /* Firefox */
+        msOverflowStyle: 'none',  /* Internet Explorer 10+ */
+    }
 
     return <>
-        <ListGroup>
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-        </ListGroup>
+        <div style={containerStyle}>
+            <ListGroup
+                style={scrollableWithoutBar}
+                children={getPlayerCards()}
+            />
+        </div>
     </>
-
 }
 
 export default ActivePlayers
--- chat-room-frontend/src/components/Profile/Profile.tsx
+++ chat-room-frontend/src/components/Profile/Profile.tsx
@@ -1,20 +1,46 @@
 import Card from 'react-bootstrap/Card';
+import titleCard2 from "../../assets/accuser.png";
+import titleCard3 from "../../assets/curiosity.png";
+import titleCard4 from "../../assets/bazzmoz.png";
+import titleCard5 from "../../assets/eureka.png";
+import titleCard6 from "../../assets/detective.png";
+import titleCard7 from "../../assets/minusone.png";
+import titleCard8 from "../../assets/robohno.png";
+import titleCard9 from "../../assets/sadgears.png";
+import titleCard10 from "../../assets/sargentbolts.png";
+import titleCard11 from "../../assets/scarytron.png";
+import titleCard12 from "../../assets/sonny.png";
+import titleCard13 from "../../assets/wattee.png";
 
-function Profile(props: {imgURL: string,  isSpectator: boolean}) {
+function Profile(props: {imgURL?: string,  isSpectator: boolean}) {
 
-    const {imgURL, isSpectator} = props;
-    let XorO;
+    let {imgURL} = props.imgURL;
+    const {isSpectator} = props.isSpectator;
 
-    (isSpectator == true) ? XorO = "X" : XorO = "O";
+    const getRandomTitleCard = () => {
 
-    return <>
-        <Card style={{ width: '200px' }}>
-        <Card.Img variant="top" src={imgURL} />
-            <Card.Text>
-                {XorO}
+        const titleCards = [titleCard2,
+            titleCard3, titleCard4, titleCard5, titleCard6,
+            titleCard7, titleCard8, titleCard9, titleCard10,
+            titleCard11, titleCard12, titleCard13,
+        ]
+
+        const index = Math.floor(Math.random() * titleCards.length);
+        return titleCards[index];
+    }
+
+    if(!imgURL){
+        imgURL = getRandomTitleCard()
+    }
+
+    return (
+        <Card style={{ width: '96px', height: '96px', padding: '5px', backgroundColor: 'grey', borderColor: '#222222', position: 'relative', margin:'.5rem' }}>
+            <img src={imgURL} alt="Profile" style={{ objectFit: 'fill', borderRadius: '1rem' }} />
+            <Card.Text style={{ fontWeight: 'bold', position: 'absolute', bottom: '-10px', left: '0', right: '0', textAlign: 'center', color: 'white', background: 'rgba(0, 0, 0, 0.5)', padding: '5px', borderRadius:'50%' }}>
+                {isSpectator ? "Spectator" : "Player"}
             </Card.Text>
         </Card>
-    </>
+    );
 }
 
 export default Profile
