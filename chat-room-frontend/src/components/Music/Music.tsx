87f6ec3d3d90153da78714ff212fd25fda3c53b8
aeharriman <aeharrima@gmail.com>
2023-05-19 15:37:59 -0500

    commented music component

--- chat-room-frontend/src/components/Music/Music.tsx
+++ chat-room-frontend/src/components/Music/Music.tsx
@@ -1,26 +1,41 @@
 import React, { useEffect, useRef } from 'react';
 
+// creates a name for the type of data that will be passed into the Music function
 interface MusicProps {
     musicSource: string;
 }
 
+// function takes in a MusicProps (aka string) named musicSource and returns a React Element
 function Music({ musicSource }: MusicProps): React.ReactElement {
+
+    // useRef is a react hook that takes a DOM element (indicated by the ref prop in the element itself)
+    // and returns an object (audioRef) whose .current prop gets set to the passed argument (null).
+    // Then you can change the properties of the object without re-rendering the component.
     const audioRef = useRef<HTMLAudioElement | null>(null);
 
+    // This is called when the value of musicSource changes.
     useEffect(() => {
+        // It takes the current values of the element it references
         const audio = audioRef.current;
         if (audio instanceof HTMLAudioElement) {
             audio.src = musicSource;
             audio.volume = 0.1;
-            const playPromise = audio.play();
-
+            // Plays audio and assigns the promise it returns a variable name
+            const playPromise: Promise<void> | undefined = audio.play();
+ 
+            // audio.play is an asynchronous function that returns a promise because of the way the browser handles audio
+            // If play was called synchronously (like autoplay) it would return undefined, which is what this checks for. This 
+            // It isn't called synchronously here, but chatGPT said it was good practice to ensure it is a promise
             if (playPromise !== undefined) {
                 playPromise
-                    .then(_ => {
-                        // Automatic playback started!
+                    // Reports back on success or failure
+                    // The _ is convention that you are aren't using the return value
+                    .then((_ : unknown) => {
+                        // playback started!
                     })
-                    .catch(error => {
-                        // Auto-play was prevented
+                    // error is a built-in object type
+                    .catch((error: Error) => {
+                        // displays an error if the play failed
                         console.log(error);
                     });
             }
