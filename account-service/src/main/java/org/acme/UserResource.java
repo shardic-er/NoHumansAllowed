ce96fa190c7f2a24cfe863dda68e7339e20e8e89
calebbethea95 <93457747+calebbethea95@users.noreply.github.com>
2023-05-28 19:34:52 -0500

    isuued jwt

--- account-service/src/main/java/org/acme/UserResource.java
+++ account-service/src/main/java/org/acme/UserResource.java
@@ -5,6 +5,7 @@ import appuser.AppUser;
 import appuser.Credentials;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import io.smallrye.jwt.build.Jwt;
 import io.vertx.core.json.JsonObject;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
@@ -79,23 +80,29 @@ public class UserResource {
 
     @POST
     @Path("/login")
-    public Response login(Credentials credentials){
+    public Response login(Credentials credentials) {
         String username = credentials.getUsername();
         String password = credentials.getPassword();
-        try{
+        try {
+            // Verify user credentials against the stored data in the database
             AppUser user = AppUser.find("username", username).firstResult();
-            if(user != null) {
-                if (password.equals(user.getPassword())) {
-                    return Response.status(Response.Status.ACCEPTED).entity(user).build();
-                }
-            }
-            return Response.status(Response.Status.BAD_REQUEST).build();
-        }
-        // Add error handling for wrong credentials requests
-        catch (Exception ex){
-            return Response.status(Response.Status.BAD_REQUEST).build();
-        }
+            if (user != null && password.equals(user.getPassword())) {
+                // Generate a JWT token for the authenticated user
+                String token = Jwt.issuer("your-issuer")
+                        .subject(user.getUsername())
+                        .claim("userId", user.getUser_id())
+                        .sign();
 
+                // Return the JWT token as a response
+                return Response.ok(token).build();
+            } else {
+                // Invalid credentials
+                return Response.status(Response.Status.UNAUTHORIZED).build();
+            }
+        } catch (Exception ex) {
+            // Error occurred during authentication
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
 }
