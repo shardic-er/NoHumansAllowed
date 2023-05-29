d6b034d3624c27f7bf0dbcd6c8c4ce443ddeed84
Shardic <eli.ronai@gmail.com>
2023-05-28 18:20:04 -0600

    added Credentials

---
+++ account-service/src/main/java/appuser/Credentials.java
@@ -0,0 +1,29 @@
+package appuser;
+
+public class Credentials {
+
+    public Credentials() {
+    }
+
+    public String username;
+
+    public String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Credentials setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Credentials setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+}
